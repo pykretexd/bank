b6e4a8f873750e092088afa949184bc0c96a3ff4
pykrete <landysixten@gmail.com>
2023-01-26 19:46:08 +0100

    Initial commit

---
+++ src/app.py
@@ -0,0 +1,18 @@
+import os
+from dotenv import load_dotenv
+from flask import Flask
+from flask_migrate import Migrate
+from model import db
+
+load_dotenv()
+database_uri = os.environ.get('DATABASE_URI')
+secret_key = os.environ.get('SECRET_KEY')
+
+app = Flask(__name__)
+migrate = Migrate(app, db)
+app.config['SQLALCHEMY_DATABASE_URI'] = str(database_uri)
+app.config['SECRET_KEY'] = str(secret_key)
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
---
+++ src/model.py
@@ -0,0 +1,43 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Customer(db.Model):
+    __tablename__= "Customers"
+    Id = db.Column(db.Integer, primary_key=True)
+    GivenName = db.Column(db.String(50), unique=False, nullable=False)
+    Surname = db.Column(db.String(50), unique=False, nullable=False)
+    Streetaddress = db.Column(db.String(50), unique=False, nullable=False)
+    City = db.Column(db.String(50), unique=False, nullable=False)
+    Zipcode = db.Column(db.String(10), unique=False, nullable=False)
+    Country = db.Column(db.String(30), unique=False, nullable=False)
+    CountryCode = db.Column(db.String(2), unique=False, nullable=False)
+    Birthday = db.Column(db.DateTime, unique=False, nullable=False)
+    NationalId = db.Column(db.String(20), unique=False, nullable=False)
+    TelephoneCountryCode = db.Column(db.Integer, unique=False, nullable=False)
+    Telephone = db.Column(db.String(20), unique=False, nullable=False)
+    EmailAddress = db.Column(db.String(50), unique=False, nullable=False)
+
+    Accounts = db.relationship('Account', backref='Customer',
+     lazy=True)
+
+class Account(db.Model):
+    __tablename__= "Accounts"
+    Id = db.Column(db.Integer, primary_key=True)
+    AccountType = db.Column(db.String(10), unique=False, nullable=False)
+    Created = db.Column(db.DateTime, unique=False, nullable=False)
+    Balance = db.Column(db.Integer, unique=False, nullable=False)
+    Transactions = db.relationship('Transaction', backref='Account',
+     lazy=True)
+    CustomerId = db.Column(db.Integer, db.ForeignKey('Customers.Id'), nullable=False)
+
+
+class Transaction(db.Model):
+    __tablename__= "Transactions"
+    Id = db.Column(db.Integer, primary_key=True)
+    Type = db.Column(db.String(20), unique=False, nullable=False)
+    Operation = db.Column(db.String(50), unique=False, nullable=False)
+    Date = db.Column(db.DateTime, unique=False, nullable=False)
+    Amount = db.Column(db.Integer, unique=False, nullable=False)
+    NewBalance = db.Column(db.Integer, unique=False, nullable=False)
+    AccountId = db.Column(db.Integer, db.ForeignKey('Accounts.Id'), nullable=False)
