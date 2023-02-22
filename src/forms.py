f2bc898cecc9c66c1d812f5d6b645456719dd85c
pykrete <landysixten@gmail.com>
2023-02-23 00:56:45 +0100

    Delete forms.py

--- src/forms.py
+++
@@ -1,7 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, DateField, BooleanField, IntegerField
-from wtforms.validators import DataRequired
-
-class AccountIdForm(FlaskForm):
-    id = StringField('ID', validators=[DataRequired()])
-    submit = SubmitField('Search')
