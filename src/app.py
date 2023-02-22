3fc769bbca94568df42e826d33ce29bc555f057c
pykrete <landysixten@gmail.com>
2023-02-22 22:24:17 +0100

    Update app.py

--- src/app.py
+++ src/app.py
@@ -15,21 +15,25 @@ app.config['SQLALCHEMY_DATABASE_URI'] = str(database_uri)
 app.config['SECRET_KEY'] = str(secret_key)
 db.init_app(app)
 
-@app.route('/', methods=['GET'], defaults={'page': 1})
-@app.route('/<int:page>', methods=['GET'])
-def index(page):
-    page = page
-    customer_count = Customer.query.count()
-    customers = Customer.query.paginate(page=page, per_page=50)
-    return render_template('index.html', customer_count=customer_count, customers=customers)
+@app.route('/')
+def index():
+    customers = Customer.query.all()
+    return render_template('index.html', customers=customers)
 
-@app.route('/profile/<int:id>', methods=['GET'])
-def profile(id):
-    customer = Customer.query.get_or_404(id)
-    return render_template('profile.html', customer=customer)
+@app.route('/customer/<int:customer_id>')
+def customer(customer_id):
+    customer = Customer.query.get_or_404(customer_id)
+    accounts = Account.query.filter_by(customer_id=customer_id).all()
+    return render_template('customer.html', customer=customer, accounts=accounts)
+
+@app.route('/customer/<int:customer_id>/account/<int:account_id>')
+def account(customer_id, account_id):
+    account = Account.query.get_or_404(account_id)
+    transactions = Transaction.query.filter_by(account_id=account_id).all()
+    return render_template('account.html', account=account, transactions=transactions)
 
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
         seed_data(db)
-        app.run()
+        app.run(debug=True)
